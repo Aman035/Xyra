21deea5b56c748f0ef074b56ba6a3ef335367ca4
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-08-31 06:16:01 +0530

    feat: added withdraw and repay feature on portfolio page

--- frontend/lib/lendingPool.ts
+++ frontend/lib/lendingPool.ts
@@ -124,6 +124,144 @@ export const supply = async (
   }
 }
 
+export const withdraw = async (
+  connectedWallet: any,
+  withdrawVaultZrc20: string,
+  withdrawAmount: bigint,
+  withdrawTokenAddress: string,
+  toChainId: number,
+  toAddress: string
+) => {
+  const toHex = isAddress(toAddress)
+    ? (toAddress as Hex)
+    : bytesToHex(base58.decode(toAddress))
+
+  const payload = utils.defaultAbiCoder.encode(
+    ['string', 'bytes', 'uint256', 'address', 'address', 'uint256'],
+    [
+      'withdraw',
+      toHex,
+      toChainId,
+      withdrawVaultZrc20,
+      withdrawTokenAddress,
+      withdrawAmount,
+    ]
+  )
+
+  if (connectedWallet.chainType === 'solana') {
+    throw new Error('Unimplemented')
+  } else {
+    const provider = await connectedWallet.getEthereumProvider()
+    const hexChainId = await provider.request({ method: 'eth_chainId' })
+    const chainId = parseInt(hexChainId, 16)
+    const walletClient = createWalletClient({
+      account: connectedWallet.address as Hex,
+      chain:
+        chainId === CHAINS.baseSepolia.id
+          ? baseSepolia
+          : chainId === CHAINS.sepolia.id
+          ? sepolia
+          : zetachainAthensTestnet,
+      transport: custom(provider),
+    })
+
+    if (chainId === CHAINS.zetaAthens.id) {
+      // const decimals = getVaultByZRC20(withdrawVaultZrc20)!.asset.decimals
+      const amount = withdrawAmount
+
+      const { receipt } = await writeContract({
+        signer: walletClient,
+        functionName: 'withdraw',
+        args: [
+          withdrawVaultZrc20,
+          amount,
+          { chainId: toChainId, identity: toHex },
+        ],
+      })
+      return receipt as TransactionReceipt
+    } else {
+      return evmCall({
+        client: walletClient,
+        gateway: '0x0c487a766110c85d301d96e33579c5b317fa4995',
+        payload: payload as `0x${string}`,
+      })
+    }
+  }
+}
+
+export const repay = async (
+  connectedWallet: any,
+  repayVaultZrc20: string,
+  nativeAmountRepaid: string,
+  onBehalfOfChainId: number,
+  onBehalfOfAddress: string
+) => {
+  const onBehalfOfHex = isAddress(onBehalfOfAddress)
+    ? (onBehalfOfAddress as Hex)
+    : bytesToHex(base58.decode(onBehalfOfAddress))
+
+  const payload = utils.defaultAbiCoder.encode(
+    ['string', 'bytes', 'uint256', 'address', 'address', 'uint256'],
+    [
+      'repay',
+      onBehalfOfHex,
+      onBehalfOfChainId,
+      repayVaultZrc20,
+      zeroAddress,
+      0,
+    ]
+  )
+
+  if (connectedWallet.chainType === 'solana') {
+    throw new Error('Unimplemented')
+  } else {
+    const provider = await connectedWallet.getEthereumProvider()
+    const hexChainId = await provider.request({ method: 'eth_chainId' })
+    const chainId = parseInt(hexChainId, 16)
+    const walletClient = createWalletClient({
+      account: connectedWallet.address as Hex,
+      chain:
+        chainId === CHAINS.baseSepolia.id
+          ? baseSepolia
+          : chainId === CHAINS.sepolia.id
+          ? sepolia
+          : zetachainAthensTestnet,
+      transport: custom(provider),
+    })
+
+    if (chainId === CHAINS.zetaAthens.id) {
+      const decimals = getVaultByZRC20(repayVaultZrc20)!.asset.decimals
+      const amount = parseUnits(nativeAmountRepaid, decimals)
+
+      await approveIfNeeded({
+        token: repayVaultZrc20 as `0x${string}`,
+        owner: walletClient.account!.address as `0x${string}`,
+        spender: process.env.NEXT_PUBLIC_LENDING_POOL as `0x${string}`,
+        amount,
+        walletClient,
+      })
+
+      const { receipt } = await writeContract({
+        signer: walletClient,
+        functionName: 'repay',
+        args: [
+          repayVaultZrc20,
+          amount,
+          { chainId: onBehalfOfChainId, identity: onBehalfOfHex },
+        ],
+      })
+      return receipt as TransactionReceipt
+    } else {
+      return evmDepositAndCall({
+        client: walletClient,
+        gateway: '0x0c487a766110c85d301d96e33579c5b317fa4995',
+        payload: payload as `0x${string}`,
+        etherAmount: nativeAmountRepaid,
+      })
+    }
+  }
+}
+
 export const borrow = async (
   connectedWallet: any,
   borrowVaultZrc20: string,
